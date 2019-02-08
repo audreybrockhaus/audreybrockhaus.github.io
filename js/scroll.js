5dcd311ccce2fa51538f9b4862e899f8e2761b0e
Audrey Brockhaus <audreybrockhaus@gmail.com>
2019-02-07 18:53:42 -0700

    completing resume page

--- js/scroll.js
+++ js/scroll.js
@@ -59,26 +59,26 @@ $(document).ready(function(){
   });
 
   // smooth anchors
-  $("a").on('click', function(event) {
+  // $("a").on('click', function(event) {
 
-    // Make sure this.hash has a value before overriding default behavior
-      var $myTarget = $(this).attr('href');
-      if ($myTarget && $myTarget.match('^#')){
-        // Prevent default anchor click behavior
-        event.preventDefault();
+  //   // Make sure this.hash has a value before overriding default behavior
+  //     var $myTarget = $(this).attr('href');
+  //     if ($myTarget && $myTarget.match('^#')){
+  //       // Prevent default anchor click behavior
+  //       event.preventDefault();
 
-        // Store hash
-        var hash = this.hash;
+  //       // Store hash
+  //       var hash = this.hash;
 
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
+  //       // Using jQuery's animate() method to add smooth page scroll
+  //       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+  //       $('html, body').animate({
+  //         scrollTop: $(hash).offset().top
+  //       }, 800, function(){
     
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
+  //       // Add hash (#) to URL when done scrolling (default click behavior)
+  //       window.location.hash = hash;
+  //     });
+  //   } // End if
+  // });
 });
